054eded2dfc5489b37b39039b0e5dbbfeefd5f16
Isabelydjoliz <124791216+Isabelydjoliz@users.noreply.github.com>
2024-02-29 08:35:15 -0300

    Add files via upload

---
+++ aula_3/js/executa.js
@@ -0,0 +1,6 @@
+var resultado = document.getElementById('resultado')
+function pegaNome(){
+    var nome = document.getElementById('nome').value
+    console.log('Nome: ', nome)
+    resultado.innerHTML = nome
+}
