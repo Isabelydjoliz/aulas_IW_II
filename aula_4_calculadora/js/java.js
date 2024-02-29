234c5371cc1e436f497ab12f512f86180800837e
Isabelydjoliz <124791216+Isabelydjoliz@users.noreply.github.com>
2024-02-29 08:35:33 -0300

    Add files via upload

---
+++ aula_4_calculadora/js/java.js
@@ -0,0 +1,28 @@
+function peganum1(){
+    var num1 = document.getElementById('num1').value
+    console.log('Número 1: ', num1)
+}
+
+function peganum2(){
+    var num2 = document.getElementById('num2').value
+    console.log('Número 2: ', num2)
+}
+
+function botao_somar(){
+    var num1 = document.getElementById('num1').value
+    var num2 = document.getElementById('num').value
+    var somando = num1 + num2 
+
+}
+
+function botao_subtrair(){
+
+}
+
+function botao_dividir(){
+
+}
+
+function bota_multiplicacao(){
+
+}
